c208e8c3175d256b16dce6849f2675c6640f9f1a
azadtom <kumarazad2917@gmail.com>
2025-07-27 19:49:08 +0530

    Add hero section with introduction text and action buttons

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,6 @@
 import NavigationWrapper from "@/components/NavigationDrawer/NavigationWrapper";
+import { DownloadIcon } from "lucide-react";
+import { ReactNode } from "react";
 
 export default function Home() {
   return (
@@ -12,7 +14,36 @@ export default function Home() {
           background:
             "radial-gradient(98.87% 98.87% at 51.11% 1.13%, rgba(0, 0, 0, 0) 0%, #000000 45.31%, #6A01D3 76.56%, #8F00FF 86.46%, #C883FF 100%)",
         }}
-      />
+      >
+        <div className="text-white flex flex-col items-center justify-center mt-[12rem] sm:mt-20">
+          <h1 className="mt-[32px] text-4xl outfit-700 font-bold tracking-tight text-[#fafafae6] text-center max-w-[600px] mx-auto">Frontend software engineer from India, focussed on building pixel perfect web apps.</h1>
+          <div className="flex flex-col sm:flex-row gap-4 items-center mt-[32px]">
+            <button className="bg-white  w-full sm:w-[170px] text-black rounded-full h-[45px] px-[24px] py-[12px]  outfit-500 flex justify-center items-center">{"Let's Talk"}</button>
+            <GradientBorderButton>
+              Get Resume
+            </GradientBorderButton>
+          </div>
+        </div>
+      </div>
     </main>
   );
 }
+
+
+const GradientBorderButton = ({ children }: { children: ReactNode }) => {
+  return (
+    <div className="w-full sm:w-[170px] inline-block p-[1px] rounded-full bg-[linear-gradient(90deg,_#FFDEDE_0%,_#F67A7A_36%,_#D4D9FF_69%,_#DC79FF_100%)]">
+      <button className="px-[24px] py-[12px] rounded-full bg-black text-white font-medium flex gap-2 items-center">
+        <DownloadIcon />{children}
+      </button>
+    </div>
+  );
+};
+
+
+
+
+
+
+
+
