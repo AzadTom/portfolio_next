4ae1fbeaa6d69b8c443309d0d26c1c0a8d1357b5
azadtom <kumarazad2917@gmail.com>
2025-08-16 13:43:35 +0530

    update WorkExperience.tsx

--- src/components/WorkExperience/WorkExperience.tsx
+++ src/components/WorkExperience/WorkExperience.tsx
@@ -1,17 +1,17 @@
 import type { ExperienceItemType } from "@/components/work-experience";
 import { WorkExperience } from "@/components/work-experience";
 
-const WORK_EXPERIENCE: ExperienceItemType[] = [
+const WORK_EXPERIENCE1: ExperienceItemType[] = [
   {
     id: "quaric",
-    companyName: "Quaric Co., Ltd.",
-    companyLogo: "https://assets.chanhdai.com/images/companies/quaric.svg",
+    companyName: "Paretune",
+    companyLogo: "https://www.parentune.com/react/assets/default/logo.png",
     positions: [
       {
         id: "30d3a9fb-021d-452a-9d27-83655369b4b9",
-        title: "Software Engineer",
+        title: "Frontend Developer",
         employmentPeriod: "03.2024 — present",
-        employmentType: "Part-time",
+        employmentType: "Full-time",
         icon: "code",
         description: `- Integrated VNPAY-QR for secure transactions.
 - Registered the e-commerce site with [online.gov.vn](https://online.gov.vn) for compliance.
@@ -19,58 +19,71 @@ const WORK_EXPERIENCE: ExperienceItemType[] = [
 - Build and maintain ZaDark.com with Docusaurus, integrating AdSense.
 - Develop and maintain the ZaDark extension for Zalo Web on Chrome, Safari, Edge, and Firefox — with 15,000+ active users via Chrome Web Store.`,
         skills: [
-          "Next.js",
-          "Strapi",
-          "Auth0",
-          "VNPAY-QR",
+          "ReactJS",
+          "TailwindCss",
+          "Responsive design",
+          "NextJS",
+          "Redux",
+          "React Query",
+          "Framer motion",
           "Docker",
-          "NGINX",
-          "Google Cloud",
-          "Docusaurus",
-          "Extension",
-          "Research",
-          "Project Management",
+          "Git",
+          "Github"
         ],
         isExpanded: true,
-      },
-      {
-        id: "7586afb2-40e8-49c4-8983-2254c9446540",
-        title: "Product Designer",
-        employmentPeriod: "03.2024 — present",
-        employmentType: "Part-time",
-        icon: "design",
-        description: `- Design UI/UX for Quaric Website with a seamless experience.
-- Develop a Design System for consistency and efficiency.
-- Create Quaric's brand identity, including logo and guidelines.`,
-        skills: [
-          "UI/UX Design",
-          "UX Writing",
-          "Design System",
-          "Brand Design",
-          "Figma",
-        ],
-      },
-      {
-        id: "991692c4-7d02-4666-8d31-933c4831768d",
-        title: "Founder & Director",
-        employmentPeriod: "03.2024 — present",
-        employmentType: "Part-time",
-        icon: "business",
-        description: `- Lead and manage the company's strategy.
-- Oversee technical teams and product development.
-- Manage relationships with customers and partners.`,
-        skills: ["Business Ownership", "Business Law", "Business Tax"],
       },
     ],
     isCurrentEmployer: true,
   },
 ];
 
+const WORK_EXPERIENCE2: ExperienceItemType[] = [
+  {
+    id: "quaric",
+    companyName: "GeeksForGeeks",
+    companyLogo: "https://media.geeksforgeeks.org/gfg-gg-logo.svg",
+    positions: [
+      {
+        id: "30d3a9fb-021d-452a-9d27-83655369b4b9",
+        title: "Trainne-(MERN)",
+        employmentPeriod: "12.2024 — 02-2025",
+        employmentType: "Trainne",
+        icon: "code",
+        description: `- Integrated VNPAY-QR for secure transactions.
+- Registered the e-commerce site with [online.gov.vn](https://online.gov.vn) for compliance.
+- Developed online ordering to streamline purchases.
+- Build and maintain ZaDark.com with Docusaurus, integrating AdSense.
+- Develop and maintain the ZaDark extension for Zalo Web on Chrome, Safari, Edge, and Firefox — with 15,000+ active users via Chrome Web Store.`,
+        skills: [
+          "ReactJS",
+          "TailwindCss",
+          "Responsive design",
+          "Redux",
+          "NodeJS",
+          "ExpressJS",
+          "MongoDB",
+          "Postman",
+          "Git",
+          "Github"
+        ],
+        isExpanded: false,
+      },
+    ],
+    isCurrentEmployer: false,
+  },
+];
+
 export function WorkExperienceDemo() {
   return (
-    <WorkExperience
-      className="max-w-[1000px] mx-auto rounded-lg  text-white"
-      experiences={WORK_EXPERIENCE}
-    />
+    <>
+      <WorkExperience
+        className="max-w-[1000px] mx-auto rounded-lg outfit-400 text-white"
+        experiences={WORK_EXPERIENCE1}
+      />
+      <WorkExperience
+        className="max-w-[1000px] mx-auto rounded-lg outfit-400 text-white"
+        experiences={WORK_EXPERIENCE2}
+      />
+    </>
   );
 }
