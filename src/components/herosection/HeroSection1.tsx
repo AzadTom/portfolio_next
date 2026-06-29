f33d529f111e0a863740515b860cf2e4ec7cde3b
azadtom <kumarazad2917@gmail.com>
2026-06-29 14:03:54 +0530

    feat: replace button elements with Button component in HeroSection1 for consistency and improved styling

--- src/components/herosection/HeroSection1.tsx
+++ src/components/herosection/HeroSection1.tsx
@@ -3,6 +3,7 @@
 import { cn } from '@/lib/utils';
 import React from 'react'
 import { Heading } from '../designsytem/DesignSytem';
+import { Button } from '../ui/button';
 
 const HeroSection1 = () => {
   return (
@@ -16,7 +17,7 @@ const HeroSection1 = () => {
                  <div className="p-10 size-full relative shadow-2xl flex flex-col justify-between">
                   <img className="absolute inset-0 w-full h-full object-cover mask-radial-from-50% mask-b-from-50% mask-t-from-20% select-none pointer-events-none" src="https://images.unsplash.com/photo-1782392455081-a09db8267afb?q=80&w=784&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="heroimg" />
                   <nav className="flex justify-end z-50 relative">
-                    <button className="px-4 py-2 rounded-md bg-gradient-to-t from-blue-700 to-blue-500 text-shadow-lg text-shadow-black/2 text-white  text-base">Try for free</button>
+                    <Button className="px-4 py-2 rounded-md bg-gradient-to-t from-blue-700 to-blue-500 text-shadow-lg text-shadow-black/2 text-white  text-base">Try for free</Button>
                   </nav>
                   <div className="flex flex-col relative z-50">
                       <Heading className="text-neutral-950">
@@ -25,8 +26,9 @@ const HeroSection1 = () => {
                       <Heading variant={"subheading"} className="text-neutral-500 mt-8">
                          Pass in a URL,and Deep cloning AI will clone the voice of the person in the credit card required.
                       </Heading>
-                      <div className="mt-8">
-                        <button className="px-4 py-2 rounded-md bg-gradient-to-t from-blue-700 to-blue-500 text-shadow-lg text-shadow-black/2 text-white  text-base cursor-pointer active:scale-98 transition-duration">Try for free</button>
+                      <div className="mt-8 flex flex-col  gap-5 md:flex-row md:items-center">
+                        <Button variant={"secondary"} className="capitalize">No Credit card required</Button>
+                        <Button className="px-4 py-2 rounded-md bg-gradient-to-t from-blue-700 to-blue-500 text-shadow-lg text-shadow-black/2 text-white  text-base cursor-pointer active:scale-98 transition-duration">Try for free</Button>
                       </div> 
                   </div>
                  <Lines className="mask-b-from-10% absolute  inset-x-0 top-0"/>
